3d1a9fd7c62c3716bdacf27b7fa7259cacb895a1
unknown <konghoitec@gmail.com>
2023-10-27 14:12:16 +0800

    increased IGNORE_KEYS keys

--- register_keypress.js
+++ register_keypress.js
@@ -1,6 +1,8 @@
 const IGNORE_KEYS = [ 
     "Shift", "Control", "Meta", "Alt", "CapsLock", "Enter", "Escape", "Tab",
-    "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"
+    "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp", "NumLock", "Delete", 
+    "Home", "End", "PageUp", "PageDown", "Insert", "F1", "F2", "F3", "F4", 
+    "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"
 ]
 
 let typingMode = true;
